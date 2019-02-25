3c1ab10653e040944a5d2aea3a34fd83ad68c4ac
Natalya Shylkina <shylkina_nn@groupbwt.com>
2019-02-26 00:58:36 +0200

    Add video to auth page background

--- src/app/components/pages/Auth.vue
+++ src/app/components/pages/Auth.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="page page--auth">
     <nav-bar />
+    <div class="container--birds">
+      <video autoplay="" loop="" class="ng-tns-c5-0" src="~assets/auth-background.mp4"></video>
+    </div>
     <auth-form class="container"/>
   </div>
 </template>
