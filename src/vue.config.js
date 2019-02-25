7410a79472855ae2e628951ac3c31d8268c77147
Natalya Shylkina <ns@albedo-tech.com>
2019-02-25 08:08:46 +0100

    add configuration to webpack

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,7 +1,51 @@
+const appPath = `${__dirname}/app`;
+const outputPath = `${__dirname}/dist`;
+
 module.exports = {
-    pages: {
-        index: {
-            entry: 'app/main.js',
-        }
+  lintOnSave: true,
+  outputDir: outputPath,
+  css: {
+    extract: {
+      filename: 'css/style.css',
+      chunkFilename: 'css/[id].css',
     },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: { '@': appPath },
+      symlinks: false,
+    },
+    entry: { app: `${appPath}/main.js` },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: file => (
+            /node_modules/.test(file)
+            && !/\.vue\.js/.test(file)
+          ),
+        },
+      ],
+    },
+    output: {
+      filename: 'js/script.js',
+      chunkFilename: 'js/[id].js',
+    },
+    plugins: [],
+  },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].hash = true;
+      return args;
+    });
+    config.plugin('copy').tap(([pathConfigs]) => {
+      pathConfigs.unshift({
+        from: `${appPath}/assets/.htaccess`,
+        to: `${outputPath}/.htaccess`,
+        toType: 'file',
+      });
+      return [pathConfigs];
+    });
+  },
 };
